e4312b7f9686afe1ce351e88e35cc47f3fc551fa
Eric <makingentropy@gmail.com>
2017-08-17 16:38:06 -0500

    basic server.js set up

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express        = require('express');
 const app            = express();
-// const mongoose       = require('mongoose');
-// const bodyParser     = require('body-parser');
+const mongoose       = require('mongoose');
+const bodyParser     = require('body-parser');
 // const methodOverride = require('method-override');
 // const session        = require('express-session');
 // require('dotenv').config();
 
+app.use(bodyParser.json());
+app.use(express.static('public'));
 // app.use(methodOverride('_method'));
-// app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended:false}));
-// app.use(express.static('public'));
+
 
 // app.use(session({
 //     secret: "this is a random string secret", //a random string do not copy this value or your stuff will get hacked
@@ -18,12 +19,17 @@ const app            = express();
 //
 // }));
 
+// app.get('/', (req, res)=>{
+// 	res.send("Hello!");
+// });
 
-app.get('/', (req, res)=>{
-	res.send("Hello!");
+const carsController = require('./controllers/usedCar.js');
+app.use('/usedCars', carsController);
+
+mongoose.connect('mongodb://localhost:27017/carscrud');
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongo');
 });
-
-
 
 app.listen(3000, ()=>{
 	console.log('listening....');
